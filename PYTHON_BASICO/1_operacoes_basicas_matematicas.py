1ef90535cb2c91cf6b807c4867c50c9b43f4c1b2
walacedelgado <walacecastro@live.com>
2025-09-23 15:54:50 -0300

    atualizando

---
+++ PYTHON_BASICO/1_operacoes_basicas_matematicas.py
@@ -0,0 +1,40 @@
+"""
+### Operações Matemáticas no Python:
+
+1. Adição -> +
+2. Subtração -> -
+3. Divisão -> /
+4. Multiplicação -> *
+5. Mod (resto da divisão) -> %
+
+###Ordem das operações matemáticas
+1.parênteses
+2.potência
+3.multiplicação
+4.divisão
+5.divisão inteira
+6.resto da divisão
+7.soma e subtração.
+
+"""
+
+#adição
+print(3 + 5)
+
+#subtração
+print(5 - 3)
+
+#divisão
+print(5 / 3)
+
+#divisão inteira
+print(5 // 3)
+
+#multiplicação
+print(5 * 3)
+
+#poteciação
+print(5 ** 2)
+
+#mod (resto da divisão de 5 por 3)
+print(5 % 3)
